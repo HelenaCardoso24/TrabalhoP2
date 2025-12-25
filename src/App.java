4715847710018fcd9f98cf17da216befbc9f3090
IvoSantos43 <->
2025-12-25 22:55:31 +0000

    .

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int opcao;
+
+        do {
+            System.out.println("1 - Criar carro");
+            System.out.println("2 - Listar carros");
+            System.out.println("0 - Sair");
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1 -> Servicos.newCarro();
+                case 2 -> Servicos.listarCarros().forEach(System.out::println);
+            }
+        } while (opcao != 0);
+
+        sc.close();
     }
 }
